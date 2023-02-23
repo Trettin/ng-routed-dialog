faec753e385f5f089c3ba82ec8ddb522718c1e48
trettin <gtcastro7@gmail.com>
2023-02-23 17:45:03 -0300

    deleting non used import

--- src/app/modules/dialog-router.module.ts
+++ src/app/modules/dialog-router.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
-import { RouterModule } from '@angular/router';
 import { DialogResolverService } from '../services/dialog-resolver.service';
 
 @NgModule({
